df0f4bde2547fb642bc01247b196db568aa5c1aa
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-05-27 01:44:16 +0600

    sent data deatabase and delete when payment success

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ async function run() {
       const menuCollection = client.db('bistroDB').collection('menu');
       const reviewsCollection = client.db('bistroDB').collection('reviews');
       const cartsCollection = client.db('bistroDB').collection('carts');
+      const paymentsCollection = client.db('bistroDB').collection('payments');
 
 
       
@@ -206,6 +207,21 @@ async function run() {
             res.send({
                   clientSecret : paymentIntent.client_secret
             })
+      })
+
+      // send payment history intent
+      app.post('/payment', async (req, res) => {
+            const payment = req.body;
+            const paymentResult = await paymentsCollection.insertOne(payment);
+
+            const query = { _id :
+                  {
+                        $in : payment.cartIds.map(id => new ObjectId(id))
+                  },
+            }
+
+            const deleteResult = await cartsCollection.deleteMany(query)
+            res.send({paymentResult, deleteResult})
       })
       
       // Send a ping to confirm a successful connection
